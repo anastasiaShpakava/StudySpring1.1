87040d352b7a7b62de5db232b23737ae996febdc
Anastasia Shpakova <shpakavaa@gmail.com>
2020-04-23 15:27:03 +0300

    corrections

---
+++ src/main/java/com/company/sh/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.company.sh.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan ("com.company.sh")
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/WEB-INF/views/**").setViewName("/views/");
+    }
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+}
--- src/main/java/com/company/sh/controller/SingerController.java
+++ src/main/java/com/company/sh/controller/SingerController.java
@@ -1,12 +1,14 @@
 package com.company.sh.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping ("/singers")
 public class SingerController {
-   @Autowired
+
 
 }
