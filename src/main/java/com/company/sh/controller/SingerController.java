e6319db1a02515ead58f484d0283a1ea674456ce
Anastasia Shpakova <shpakavaa@gmail.com>
2020-04-29 17:56:42 +0300

    corrections

--- src/main/java/com/company/sh/controller/SingerController.java
+++ src/main/java/com/company/sh/controller/SingerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
-import java.util.Objects;
+
 
 
 @Controller
